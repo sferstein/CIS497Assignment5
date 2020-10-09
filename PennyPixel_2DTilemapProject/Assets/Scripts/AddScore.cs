7e24e4b6393c9cb36bd5e5027ced4cf5e49c8d21
sferstein <57074114+sferstein@users.noreply.github.com>
2020-10-08 22:37:05 -0500

    2D Game Done

---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/AddScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Sam Ferstein
+ * Assignment 5
+ * This adds the score when the player collects a coin.
+ */
+
+public class AddScore : MonoBehaviour
+{
+    private UIManager uIManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D theCollider)
+    {
+        if (theCollider.CompareTag("Player"))
+        {
+            uIManager.score++;
+        }
+    }
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Sam Ferstein
+ * Assignment 5
+ * This manages the UI.
+ */
+
+public class UIManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (scoreText == null)
+        //{
+        //    scoreText = FindObjectOfType<Text>();
+        //}
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            scoreText.text = "Score: " + score;
+        }
+        if (score >= 10)
+        {
+            gameOver = true;
+            scoreText.text = "You Win!";
+        }
+    }
+}
